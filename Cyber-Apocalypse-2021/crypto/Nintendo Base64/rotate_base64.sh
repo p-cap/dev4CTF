3a09ea23f4bb5ae19fd89559577d0f7d1af882b7
Paul Capili <64616459+p-cap@users.noreply.github.com>
2021-04-24 14:07:12 -1000

    Create rotate_base64.sh

---
+++ Cyber-Apocalypse-2021/crypto/Nintendo Base64/rotate_base64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z $1 ]] 
+then
+	echo "Usage: rotate_base64 -f FILENAME"
+	echo "== Decodes base64 encoded string 8 times =="
+fi
+
+while getopts f:h flag
+do
+    case "${flag}" in
+        f) filename=${OPTARG}
+			  encoded_string=$(cat $filename)
+			  for d in {1..8}
+			  do 
+			  encoded_string=$(echo $encoded_string | base64 -d)
+			  done
+			  echo "The flag is $encoded_string"
+			  ;;
+
+		  h) echo "Usage: rotate_base64 -f FILENAME";;		  
+		
+		  *) echo "Usage: rotate_base64 -f FILENAME";;
+    esac
+done
